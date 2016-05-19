649c00184dac681bec338b7968b112488733957b
Дмитрий Самсонов <Dis1092006@gmail.com>
2016-05-19 05:47:09 +0300

    Добавлена возможность выхода.

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -16,4 +16,12 @@ export class AuthService {
             .map(response => response.json())
             .catch(error => Observable.throw(error.json()))
     }
+
+    logout() {
+        localStorage.clear();
+    }
+
+	isLoggedIn() {
+		return localStorage.getItem('token') !== null;
+	}
 }
